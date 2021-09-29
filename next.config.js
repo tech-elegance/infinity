55f7aba561166aeb01451b75a6eb6b2e525b3c7f
tech-elegance <82868810+tech-elegance@users.noreply.github.com>
2021-09-29 13:49:28 +0700

    0.8

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   env: {
     BACK_END_URL:
-      "https://e80f-2405-9800-ba02-1eb6-49cd-c75f-787c-a86e.ap.ngrok.io",
+      "http://cms.lifesmart.jkshat.com:8080",
     PROJECT_TEST_ID: "6140ddc0e3f75b06081c0d3c",
   },
   images: {
